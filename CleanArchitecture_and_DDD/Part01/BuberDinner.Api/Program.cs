1a8b78d874ba29c0d45677ecb89aa31a769c3697
hhko <hyungho.ko@gmail.com>
2022-08-01 23:20:58 +0900

    코드 추가

--- CleanArchitecture_and_DDD/Part01/BuberDinner.Api/Program.cs
+++ CleanArchitecture_and_DDD/Part01/BuberDinner.Api/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
